b1501f1daefd8dc3901508e20e963bfaea5157c1
Bruno da Rosa Faller <102246452+fallerbruno@users.noreply.github.com>
2023-06-23 10:55:33 -0300

    feat: :sparkles: project done

---
+++ readme.txt
@@ -0,0 +1,1 @@
+Desafios da RocketSeat da trilha de React
---
+++ todoList/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+import "./global.css";
+import { List } from "./components/List";
+function App() {
+
+  return (
+    <>
+      <Header />
+      <List />
+    </>
+  )
+}
+
+export default App
