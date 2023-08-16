3af141c04561e24f05482d5135e534ec7dd93ac5
Bruno da Rosa Faller <102246452+fallerbruno@users.noreply.github.com>
2023-08-16 18:21:12 -0300

    feat: :sparkles: doing the projext

--- ignite-shop/src/pages/_app.tsx
+++ ignite-shop/src/pages/_app.tsx
@@ -1,20 +1,36 @@
 import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import logo from "../assets/logo.svg";
-import { Container, Header } from "@/styles/pages/app";
+import { Container, Header, SideBarContainer } from "@/styles/pages/app";
 import Image from "next/image";
 import { SkeletonTheme } from "react-loading-skeleton";
+import { Handbag, X } from "@phosphor-icons/react";
+import { useState } from "react";
 // funciona como container da aplicacao e eh carregado junto com qualquer pagina da aplicação
 // colocar fora para nao carregar todas vezes, pois elas sao unicas e nao
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [open, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
     <Container>
       <SkeletonTheme baseColor="#00b37e" highlightColor="#e1e1e6">
         <Header>
           <Image src={logo} alt="logo" />
+          {!open && <Handbag size={32} onClick={handleOpen} />}
         </Header>
+        {/* <SideBarContainer>
+          <h1>SideBar</h1>
+        </SideBarContainer> */}
         <Component {...pageProps} />
       </SkeletonTheme>
     </Container>
--- ignite-shop/src/styles/pages/app.ts
+++ ignite-shop/src/styles/pages/app.ts
@@ -1,16 +1,35 @@
 import { styled } from "..";
 
-export const Container = styled('div', {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    minHeight: '100vh',
-})
+export const Container = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "center",
+  minHeight: "100vh",
+});
 
-export const Header = styled('header', {
-    padding: '2rem 0',
-    width: '100%',
-    maxWidth: '1180px',
-    margin: '0 auto',
-})
+export const Header = styled("header", {
+display: "flex",
+  padding: "2rem 0",
+  width: "100%",
+  maxWidth: "1180px",
+  margin: "0 auto",
+  justifyContent: "space-between",
+});
+
+export const SideBarContainer = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  maxWidth: 480,
+  width: "100%",
+  background: "$gray800",
+  border: "1px solid $gray-800",
+
+  position: "absolute",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 1,
+});
